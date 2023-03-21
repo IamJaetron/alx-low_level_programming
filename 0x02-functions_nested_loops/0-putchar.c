aba0a37dfcda934a4490c1f357dd1bb73112fc6a
IamJaetron <jediddy27@gmail.com>
2023-03-21 12:07:27 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * main - debugging example
- * Return: 0
+ * main - main function
+ * _putchar: print the alphabets putchar
+ * Return: always 0
  */
 int main(void)
 {
-	char *holberton = "Holberton";
 
-	while (*holberton)
-	{
-		_putchar(*holberton);
-		holberton++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
